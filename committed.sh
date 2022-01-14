28ad26ba3c9b68a89bce1fd7350e8d63cf4e4bf9
Harvey Nwynn <harveynwynn@gmail.com>
2022-01-14 12:34:32 -0800

    update files

--- committed.sh
+++ committed.sh
@@ -3,7 +3,7 @@ NUMBER=$(($RANDOM % 23))
 
 for ((run=1; run <= NUMBER + 1; run++))
 do
-  echo `/usr/games/fortune` > ~/greenery/file.txt
-  /usr/bin/git -C ~/greenery/ add . -A
-  /usr/bin/git -C ~/greenery/ commit -m "`/usr/games/fortune -sn 32`"
+  echo `/usr/games/fortune` > ~/github-square/file.txt
+  /usr/bin/git -C ~/github-square/ add . -A
+  /usr/bin/git -C ~/github-square/ commit -m "`/usr/games/fortune -sn 32`"
 done
